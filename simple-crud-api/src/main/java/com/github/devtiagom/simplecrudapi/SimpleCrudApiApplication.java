b7dcfb9817061f033ff17e669e0ce16c41cf51d9
devtiagom <tiago.o.machado@gmail.com>
2020-07-25 19:45:08 -0300

    spring boot project API initialized

---
+++ simple-crud-api/src/main/java/com/github/devtiagom/simplecrudapi/SimpleCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.devtiagom.simplecrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrudApiApplication.class, args);
+	}
+
+}
