60c4518d949f668cbaf96e3c1cab8790963c1ad3
devtiagom <tiago.o.machado@gmail.com>
2020-07-26 00:06:21 -0300

    mapped category and product relationship

--- simple-crud-api/src/main/java/com/github/devtiagom/simplecrudapi/domain/CategoryDomain.java
+++ simple-crud-api/src/main/java/com/github/devtiagom/simplecrudapi/domain/CategoryDomain.java
@@ -1,20 +1,39 @@
 package com.github.devtiagom.simplecrudapi.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@EqualsAndHashCode
 @Entity
 @Table(name = "categories")
-public class CategoryDomain {
+public class CategoryDomain implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter
     private Long id;
+
+    @Setter
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Set<ProductDomain> products = new HashSet<>();
+
+    public CategoryDomain(String name) {
+        this.name = name;
+    }
+
+    public Set<ProductDomain> addProduct(ProductDomain product) {
+        this.products.add(product);
+        return this.products;
+    }
 }
--- simple-crud-api/src/main/java/com/github/devtiagom/simplecrudapi/domain/ProductDomain.java
+++ simple-crud-api/src/main/java/com/github/devtiagom/simplecrudapi/domain/ProductDomain.java
@@ -1,24 +1,41 @@
 package com.github.devtiagom.simplecrudapi.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 @Entity
 @Table(name = "products")
-public class ProductDomain {
+public class ProductDomain implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String description;
     private BigDecimal price;
     private Integer stock;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private CategoryDomain category;
+
+    public ProductDomain(String name, String description, BigDecimal price, Integer stock, CategoryDomain category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+    }
 }
